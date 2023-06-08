d3bc52bc58b3a011882ea3cabb31f7b2ead1b722
ethanhome <32770708+ethanhome@users.noreply.github.com>
2023-06-08 14:54:34 +0800

    Update treesitter.lua
    use foldmethod=syntax

--- lua/modules/configs/editor/treesitter.lua
+++ lua/modules/configs/editor/treesitter.lua
@@ -1,8 +1,10 @@
 return vim.schedule_wrap(function()
 	local use_ssh = require("core.settings").use_ssh
 
-	vim.api.nvim_set_option_value("foldmethod", "expr", {})
-	vim.api.nvim_set_option_value("foldexpr", "nvim_treesitter#foldexpr()", {})
+	-- vim.api.nvim_set_option_value("foldmethod", "expr", {})
+	-- vim.api.nvim_set_option_value("foldexpr", "nvim_treesitter#foldexpr()", {})
+	vim.api.nvim_set_option_value("foldmethod", "syntax", {})
+	vim.api.nvim_set_option_value("foldlevelstart", 99, {})
 
 	require("nvim-treesitter.configs").setup({
 		ensure_installed = {
